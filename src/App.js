e67c417884db7afcb8b0471e02ea35073ababb10
Jeral <jeral.cris@gmail.com>
2021-12-21 22:38:05 -0600

    funcion para consultar en la API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react';
 import Formulario from './components/Formulario';
 import Header from './components/Header';
 
 function App() {
+   // state del formulario
+   const [busqueda, setBusqueda] = useState({
+      ciudad: '',
+      pais: '',
+   });
+
+   const [consultar, setConsultar] = useState(false);
+
+   const { ciudad, pais } = busqueda;
+
+   useEffect(() => {
+      const consultarApi = async () => {};
+      consultarApi();
+   }, [consultar]);
+
    return (
       <>
          <Header titulo='Clima React App' />
@@ -10,7 +26,12 @@ function App() {
             <div className='container'>
                <div className='row'>
                   <div className='col m6 s12'>
-                     <Formulario />
+                     <Formulario
+                        //
+                        busqueda={busqueda}
+                        setBusqueda={setBusqueda}
+                        setConsultar={setConsultar}
+                     />
                   </div>
 
                   <div className='col m6 s12'>1</div>
