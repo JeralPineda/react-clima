4424af3fb73e0a09b1be490dcfb05d3789771769
Jeral <jeral.cris@gmail.com>
2021-12-21 21:39:32 -0600

    Creado el formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import Formulario from './components/Formulario';
 import Header from './components/Header';
 
 function App() {
    return (
       <>
          <Header titulo='Clima React App' />
+
+         <div className='contenedor-form'>
+            <div className='container'>
+               <div className='row'>
+                  <div className='col m6 s12'>
+                     <Formulario />
+                  </div>
+
+                  <div className='col m6 s12'>1</div>
+               </div>
+            </div>
+         </div>
       </>
    );
 }
