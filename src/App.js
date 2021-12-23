7ba825d61e2988c7309a6ae7d80b82bfc79dcaba
Jeral <jeral.cris@gmail.com>
2021-12-22 23:15:09 -0600

    Actualizando la url de la API a https

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       const consultarApi = async () => {
          if (consultar) {
             const appId = process.env.REACT_APP_API_KEY;
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
             const respuesta = await fetch(url);
             const resultado = await respuesta.json();
